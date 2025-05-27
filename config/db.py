29fa96a34f4cc22c44c9500812bd68d5786eb515
yuzuteaaa <abdulmuhithnawawi@gmail.com>
2025-05-27 13:21:39 +0700

    edit ab.py

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb+srv://abdulmuhith:RSOhuo1jLS5l0aei@cluster0.5ha8rrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    db = client["sign_language_db"]
+    client = MongoClient("mongodb+srv://baisyufan:nzPoNISBiZxLIKbn@bais.5y9mhli.mongodb.net/")
+    db = client["backend-capstone"]
     return db
